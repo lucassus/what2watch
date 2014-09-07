e9f8a3a5008f8a05a176787a57150823498a4292
Lukasz Bandzarewicz <lucassus@gmail.com>
2014-09-07 16:56:53 +0200

    feat: use OpenSubtitles API to get move details

--- README.md
+++ README.md
@@ -4,13 +4,10 @@ phase 1: gather files details
 => [{ file: { name, size, hash, downloadedAt } }]
 
 phase 2: get info from OpenSubtitles
-=> [{ file, os: { imdbId, title, year } }]
+=> [{ file, movie: { imdbId, title, year } }]
 
-phase 3: scrap imdb
-=> [{ file, os, imdb: { ... } }]
-
-phase 4: merge results (file, is, imdb)
-=> [{ file: { name, downloadedAt }, title, year, rating, ... }]
+phase 3: get movie details
+=> [{ file, movie: { ... } }]
 
 phase 5: sort/filter results
 
@@ -19,6 +16,6 @@ phase 6: display results
 ## cmd line interface
 
 what2watch [path]
-  --sortby rating/year/file.downloadedAt/etc.
+  --sortBy rating/year/file.downloadedAt/etc.
   --genre drama,comedy/animation/etc
   --no-genre musical
--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var fs = require('fs');
 var path = require('path');
 var Q = require('q');
 var hasher = require('./lib/hasher');
-var request = require('request');
-var cheerio = require('cheerio');
 
 var list = function(val) {
   return _(val.split(',')).map(function(format) {
@@ -18,7 +16,7 @@ var list = function(val) {
 
 program
   .version('0.0.1')
-  .option('-f, --formats [formats]', 'list of movie formats to process, default avi,mkv,mpg', list)
+  .option('-f, --formats [formats]', 'list of movie formats to process, default avi,mkv,mp4,mpg', list)
   .option('-d, --directory [directory]', 'working directory')
   .parse(process.argv);
 
@@ -26,210 +24,193 @@ if (!program.formats) {
   program.formats = ['avi', 'mkv', 'mp4', 'mpg'];
 }
 
-var promisify = require('./lib/promisify');
+var findMovies = function(directory) {
+  var deferred = Q.defer();
 
-var listMovies = function(cwd) {
   var pattern = '**/*.+(' + program.formats.join('|') +  ')';
-  return promisify(glob)(pattern, { cwd: cwd, dot: false });
+  glob(pattern, { cwd: directory, dot: false }, function(err, files) {
+    if (err) { return deferred.reject(err); }
+
+    if (files.length > 0) {
+      deferred.resolve(files);
+    } else {
+      deferred.reject('No movies found!');
+    }
+  });
+
+  return deferred.promise;
 };
 
 var fileHash = function(filePath) {
   return Q.promise(function(resolve) {
-    hasher.getHash(filePath).done(function(hash) {
-      resolve(hash);
+    hasher.getHash(filePath).done(resolve);
+  });
+};
+
+var fileStats = function(filePath) {
+  return Q.promise(function(resolve, reject) {
+    fs.stat(filePath, function(err, stats) {
+      if (err) { return reject(err); }
+
+      resolve({
+        file: {
+          path: filePath,
+          size: stats.size,
+          downloadedAt: stats.ctime
+        }
+      });
+    });
+  }).then(function(stats) {
+    return fileHash(stats.file.path).then(function(hash) {
+      stats.file.hash = hash;
+      return stats;
     });
   });
 };
 
 var collectFileStats = function(files) {
-  var completed = 0;
-
-  var promise = Q.all(
-    _.map(files, function(file) {
+  return Q.promise(function(resolve, reject, progress) {
+    var promises = _.map(files, function(file) {
       var filePath = path.join(program.directory, file);
 
-      return Q.promise(function(resolve, reject, progress) {
-        fs.stat(filePath, function(err, stats) {
-          progress({ total: files.length, complete: ++completed });
-
-          if (err) { return reject(err); }
-
-          resolve({
-            filePath: filePath,
-            size: stats.size,
-            downloadedAt: stats.ctime
-          });
+      return fileStats(filePath)
+        .catch(reject)
+        .finally(function() {
+          progress({ total: files.length, file: file })
         });
-      });
-    })
-  );
+    });
 
-  return promise.then(function(stats) {
-    return Q.all(
-      _.map(stats, function(stat) {
-        return fileHash(stat.filePath).then(function(hash) {
-          return _.extend(stat, { hash: hash });
-        });
-      })
-    )
+    return Q.all(promises).then(resolve);
   });
 };
 
 var OpenSubtitles = require('./lib/openSubtitles');
 var api = new OpenSubtitles();
 
-var collectOpenSubtitles = function(stats) {
-  return api.login().then(function(token) {
-    var promise = Q.allSettled(
-      _.map(stats, function(stat) {
-        return Q.promise(function(resolve, reject, progress) {
-          api.checkMovieHash(stat).then(function(data) {
-            progress({ total: stats.length });
-            resolve(data);
-          }).catch(function(err) {
-            progress({ total: stats.length });
-            reject(err);
-          }).done();
+var collectInfo = function(stats) {
+  var mapping = { title: 'MovieName', imdbId: 'MovieImdbID', year: 'MovieYear', kind: 'MovieKind' };
+
+  var promise = Q.promise(function(resolve, reject, progress) {
+    api.login().then(function() {
+      var promises = _.map(stats, function(stat) {
+        var promise = api.checkMovieHash(stat.file.hash);
+
+        return promise.then(function(result) {
+          var details =_.chain(mapping)
+            .pairs()
+            .map(function(pair) { return [pair[0], result[pair[1]]] })
+            .zipObject()
+            .value();
+
+          _.extend(stat, { movie: details });
+        }).finally(function() {
+          progress({ total: stats.length });
         });
-      })
-    );
-
-    return promise.then(function(results) {
-      results = _.chain(results)
-        .select({ state: 'fulfilled' })
-        .collect(function(result) { return result.value })
-        .value();
-
-      _.each(results, function(result) {
-        var getParam = function(name) {
-          var param = _.find(result, { name: name });
-          return param.value.string;
-        };
-
-        var stat = _.find(stats, { hash: getParam('MovieHash') });
-        if (stat) {
-          var openSubtitles = {
-            title: getParam('MovieName'),
-            imdbId: getParam('MovieImdbID'),
-            year: getParam('MovieYear'),
-            kind: getParam('MovieKind'),
-            seriesSeason: getParam('SeriesSeason'),
-            seriesEpisode: getParam('SeriesEpisode'),
-            seenCount: getParam('SeenCount')
-          };
-
-          _.extend(stat, { openSubtitles: openSubtitles });
-        }
       });
 
-      return stats;
+      return Q.allSettled(promises).then(resolve);
     });
   });
+
+  return promise.thenResolve(stats);
 };
 
-var collectImdb = function(stats) {
-  var statsWithId = _.select(stats, function(stat) { return stat.openSubtitles });
+var collectDetails = function(stats) {
+  var promise = Q.promise(function(resolve, reject, progress) {
+    api.login().then(function() {
+      var promises = _.map(stats, function(stat) {
+        var promise = stat.movie && stat.movie.imdbId ?
+          api.getIMDBMovieDetails(stat.movie.imdbId) :
+          Q.reject('missing');
 
-  var promise = Q.allSettled(
-    _.map(statsWithId, function(stat) {
-
-      return Q.promise(function(resolve, reject, progress) {
-
-        var url = 'http://www.imdb.com/title/tt0' + stat.openSubtitles.imdbId;
-        request.get(url, function(err, response, body) {
-          progress();
-          if (err) { return reject(err); }
-
-          var $ = cheerio.load(body);
-          var details = {
-            id: stat.openSubtitles.imdbId,
-            url: url,
-            title: $('h1 span.itemprop[itemprop="name"]').text().trim(),
-
-            // TODO not always present
-            titleExtra: $('h1 span.title-extra[itemprop="name"]').text().trim(),
-
-            duration: $('.infobar time[itemprop="duration"]').text().trim(),
-            genre: _.collect($('.infobar a span[itemprop="genre"]'), function(el) {
-              return $(el).text().trim();
-            }),
-            rating: parseFloat($('span[itemprop="ratingValue"]').text().trim()),
-            // TODO scrap rating count
-            description: $('p[itemprop="description"]').text().trim()
-
-            // TODO scrap reviews count
-            // TODO scrap critic reviews count
-            // TODO scrap creators / director
-            // TODO scrap stars
-            // TODO parse awards
-            // TODO scrap country
-          };
-
-          resolve(details);
+        return promise.then(function(details) {
+          _.extend(stat.movie, details);
+        }).finally(function() {
+          progress({ total: stats.length });
         });
       });
-    })
-  );
 
-  return promise.then(function(results) {
-    results = _.chain(results)
-      .select({ state: 'fulfilled' })
-      .collect(function(result) { return result.value })
-      .value();
-
-    _.each(results, function(result) {
-      var stat = _.find(stats, function(stat) { return stat.openSubtitles && stat.openSubtitles.imdbId === result.id });
-      if (stat) {
-        _.extend(stat, { imdb: result });
-      }
+      return Q.allSettled(promises).then(resolve);
     });
-
-    return stats;
   });
+
+  return promise.thenResolve(stats);
 };
 
 var ProgressBar = require('progress');
 
 Q(program.directory).then(function(directory) {
-  return listMovies(directory);
+  return findMovies(directory);
 }).then(function(files) {
-  console.log('Found', files.length, 'files');
+  console.log('Found', files.length, 'movies');
 
-  var bar = new ProgressBar('Collecting file stats         [:bar] :percent :etas', { total: files.length });
+  var bar = new ProgressBar('Collecting file stats            [:bar] :percent :etas', { total: files.length });
   return collectFileStats(files).progress(function() {
     bar.tick();
   });
 }).then(function(stats) {
-  var bar = new ProgressBar('Collecting OpenSubtitles data [:bar] :percent :etas', { total: stats.length });
-  return collectOpenSubtitles(stats).progress(function() {
+  var bar = new ProgressBar('Collecting OpenSubtitles data    [:bar] :percent :etas', { total: stats.length });
+  return collectInfo(stats).progress(function() {
     bar.tick();
   });
 }).then(function(stats) {
-  var bar = new ProgressBar('Collecting Imdb data          [:bar] :percent :etas', { total: stats.length });
-  return collectImdb(stats).progress(function() {
+  var bar = new ProgressBar('Collecting OpenSubtitles details [:bar] :percent :etas', { total: stats.length });
+  return collectDetails(stats).progress(function() {
     bar.tick();
   });
 }).then(function(stats) {
-  // sort movies by year
+  // filter, sort etc.
   return _.chain(stats)
-    .sortBy(function(stat) { return stat.imdb && stat.imdb.rating })
+    .select(function(stat) { return stat.movie; } )
+    .select(function(stat) { return stat.movie.kind !== 'episode'; })
+    .sortBy(function(stat) { return parseFloat(stat.movie.rating) })
     .reverse()
     .value();
 }).then(function(stats) {
   console.log('\n\nMovie details --------------\n');
 
   _.each(stats, function(stat) {
-    if (stat.openSubtitles && stat.imdb) {
-      console.log('Title:', stat.openSubtitles.title);
-      console.log('Description:', stat.imdb.description);
-      console.log('Rating', stat.imdb.rating);
-      console.log('Genre:', stat.imdb.genre.join(', '));
-      console.log('Year:', stat.openSubtitles.year);
-      console.log('Url:', stat.imdb.url);
-      console.log('File:', stat.filePath);
+    if (stat.movie) {
+      console.log('Title:', stat.movie.title);
+      console.log('Kind:', stat.movie.kind);
+
+      console.log('Rating', stat.movie.rating);
+      console.log('Plot:', stat.movie.plot);
+      console.log('Trivia:', stat.movie.trivia);
+      console.log('Tagline:', stat.movie.tagline);
+
+      console.log('Year:', stat.movie.year);
+      console.log('Duration:', stat.movie.duration);
+
+      if (stat.movie.genres) {
+        console.log('Genre:', _.map(stat.movie.genres, function(genre) { return genre.trim() }).join(', '));
+      }
+
+      if (stat.movie.cast) {
+        console.log('Cast:', _.values(stat.movie.cast).join(', '));
+      }
+
+      if (stat.movie.directors) {
+        console.log('Directors:', _.values(stat.movie.directors).join(', '));
+      }
+
+      if (stat.movie.country) {
+        console.log('Country:', stat.movie.country.join(', '));
+      }
+
+      if (stat.movie.language) {
+        console.log('Language:', stat.movie.language.join(', '));
+      }
+
+      if (stat.movie.imdbId) {
+        console.log('Url:', 'http://www.imdb.com/title/tt0' + stat.movie.imdbId);
+      }
+
+      console.log('File:', stat.file.path);
       console.log('\n----------------------------\n');
     }
   });
 }).catch(function(err) {
   console.log('err:', err);
+  process.exit(1);
 }).done();
