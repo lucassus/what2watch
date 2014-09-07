f62e62e8ecf46bfffbca0dbef13657d39d5cffe7
Lukasz Bandzarewicz <lucassus@gmail.com>
2014-09-07 23:47:13 +0200

    feat: better formatting

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var Q = require('q');
+
 var ProgressBar = require('progress');
 
 var program = require('./lib/program');
@@ -20,6 +21,7 @@ var findMovies = require('./lib/findMovies');
 var collectFileStats = require('./lib/collectFileStats');
 var collectInfo = require('./lib/collectInfo');
 var collectDetails = require('./lib/collectDetails');
+var printMovie = require('./lib/printMovie');
 
 var buildProgressBar = function(format, total) {
   var bar = new ProgressBar(format, { total: total });
@@ -43,55 +45,12 @@ Q(program.directory).then(function(directory) {
 }).then(function(stats) {
   // filter, sort etc.
   return _.chain(stats)
-    .select(function(stat) { return stat.movie; } )
-    .select(function(stat) { return stat.movie.kind !== 'episode'; })
+    .select(function(stat) { return stat.movie && stat.movie.kind !== 'episode'; })
     .sortBy(function(stat) { return parseFloat(stat.movie.rating) })
     .reverse()
     .value();
 }).then(function(stats) {
-  console.log('\n\nMovie details --------------\n');
-
-  _.each(stats, function(stat) {
-    if (stat.movie) {
-      console.log('Title:', stat.movie.title);
-      console.log('Kind:', stat.movie.kind);
-
-      console.log('Rating', stat.movie.rating);
-      console.log('Plot:', stat.movie.plot);
-      console.log('Trivia:', stat.movie.trivia);
-      console.log('Tagline:', stat.movie.tagline);
-
-      console.log('Year:', stat.movie.year);
-      console.log('Duration:', stat.movie.duration);
-
-      if (stat.movie.genres) {
-        console.log('Genre:', _.map(stat.movie.genres, function(genre) { return genre.trim() }).join(', '));
-      }
-
-      if (stat.movie.cast) {
-        console.log('Cast:', _.values(stat.movie.cast).join(', '));
-      }
-
-      if (stat.movie.directors) {
-        console.log('Directors:', _.values(stat.movie.directors).join(', '));
-      }
-
-      if (stat.movie.country) {
-        console.log('Country:', stat.movie.country.join(', '));
-      }
-
-      if (stat.movie.language) {
-        console.log('Language:', stat.movie.language.join(', '));
-      }
-
-      if (stat.movie.imdbId) {
-        console.log('Url:', 'http://www.imdb.com/title/tt0' + stat.movie.imdbId);
-      }
-
-      console.log('File:', stat.file.path);
-      console.log('\n----------------------------\n');
-    }
-  });
+  _.each(stats, printMovie);
 }).catch(function(err) {
   console.log('err:', err);
   process.exit(1);
