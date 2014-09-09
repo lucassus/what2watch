3a651924ec25a1543771121a8b5ce4b90ffac9f4
Lukasz Bandzarewicz <lucassus@gmail.com>
2014-09-09 10:25:25 +0200

    chore(test): setup mocha, istanbul and write spec for the file hashed
    References #8

---
+++ test/hasherSpec.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+var path = require('path');
+
+describe('hasher module', function() {
+  var hasher = require('../lib/hasher');
+
+  it('calculates file hash', function(done) {
+    var file = path.join(__dirname, 'fixtures', 'one.txt');
+
+    hasher.getHash(file).done(function(hash) {
+      expect(hash).to.eq('14cadce2');
+      done();
+    });
+  });
+
+  it('calculates file hash', function(done) {
+    var file = path.join(__dirname, 'fixtures', 'two.txt');
+
+    hasher.getHash(file).done(function(hash) {
+      expect(hash).to.eq('14deeeec');
+      done();
+    });
+  });
+});
